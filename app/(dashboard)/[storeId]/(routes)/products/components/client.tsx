fb66163bccb3d877fe29bdbee84bb4d808ca507f
Sakil Khan <sakilk130@gmail.com>
2023-07-22 00:52:47 +0600

    feat: search product

--- app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
@@ -35,7 +35,7 @@ const ProductClient: FC<ProductClientProps> = ({ data }) => {
         </Button>
       </div>
       <Separator />
-      <DataTable columns={columns} data={data} searchKey="label" />
+      <DataTable columns={columns} data={data} searchKey="name" />
       <Heading title="API" description="API Calls for Products" />
       <Separator />
       <ApiList entityName="products" entityIdName="productId" />
